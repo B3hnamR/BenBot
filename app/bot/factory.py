c82a8280caca951392146c2a54485c94bb7279ea
B3hnAM <br.1515@yahoo.com>
2025-09-26 11:07:20 +0200

    update
    update

--- app/bot/factory.py
+++ app/bot/factory.py
@@ -1,11 +1,13 @@
-﻿from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher
+from aiogram.client.bot import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from app.core.config import Settings
 
 
 def create_bot(settings: Settings) -> Bot:
-    return Bot(token=settings.bot_token, parse_mode="HTML")
+    default_props = DefaultBotProperties(parse_mode="HTML")
+    return Bot(token=settings.bot_token, default=default_props)
 
 
 def create_dispatcher() -> Dispatcher:
